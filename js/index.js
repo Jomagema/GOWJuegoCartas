f93e908c6d13c9ccad13f5bb4b336bb84eb43e6c
Jose María Gestal <jmgestal00>
2023-11-24 21:52:59 +0100

    Primera version

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// JavaScript para agregar la clase 'show-image' después de que la página ha cargado
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.classList.add('show-image');
+});
